5d9c181fdba68626bd277609e18e30ded9c44048
Aaron Utterback <Autterback3@mail.greenriver.edu>
2021-01-15 15:22:45 -0500

    Added the index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// turn on error reporting
+ini_set('display_error', 1);
+error_reporting(E_ALL);
+
+ ## Aaron Utterback , Jessica Sestak
+ ## January 15, 2021
+ ##
+?>
+
+<?php
+    echo "PHP Array Practice";
+?>
+
+
+
