6e47abf4190066180a685e52b60e9523221562b6
Aaron Utterback <Autterback3@mail.greenriver.edu>
2021-01-15 18:50:43 -0500

    finished largest function

--- include/functions.php
+++ include/functions.php
@@ -1,7 +1,19 @@
 <?php
 
-function printArray($numbers){
-    foreach($numbers as $value){
+function printArray($array){
+    foreach($array as $value){
         echo "$value <br>";
     }
+}
+
+function largest($array){
+    $maxValue = $array[0];
+    $len = count($array);
+
+    for($i = 1; $i <= $len; $i++){
+        if($array[$i] > $maxValue){
+            $maxValue = $array[$i];
+        }
+    }
+    return $maxValue;
 }
--- index.php
+++ index.php
@@ -14,7 +14,13 @@ include("include/functions.php");
     echo "PHP Array Practice <br>";
     $numbers = array(7, 9, 8, 9 ,8, 8, 6);
 
+    // print the values in the numbers array
     printArray($numbers);
+    echo "<br>";
+
+    // print the largest value in the numbers array
+    echo largest($numbers);
+    echo "<br>";
 ?>
 
 
